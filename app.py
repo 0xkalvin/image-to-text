fd8fa742005398ffb9f7c01d470de62f0827bfe4
kalvin <kalvinvini1998@gmail.com>
2019-04-15 19:26:41 -0300

    id

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import urllib
 import os, os.path
 
 def get_text_from_image(url):
-    id = len([name for name in os.listdir('.') if os.path.isfile(name)])
-    urllib.request.urlretrieve(url, "0000000"+ str(id) +".jpg")
-    text = image_to_string(Image.open("0000000"+ str(id) +".jpg"), lang='eng')
+    id = str(len([name for name in os.listdir('.') if os.path.isfile(name)]) + 1 ) 
+    urllib.request.urlretrieve(url, id + ".jpg")
+    text = image_to_string(Image.open(id + ".jpg"), lang='eng')
     return text
 
 
